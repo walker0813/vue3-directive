3b07a545654858446dfac93e73991ccb1f9a2d64
ltw <862672788@qq.com>
2022-06-17 00:49:39 +0800

    v-lazy图片懒加载(IntersectionObserver)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="box" v-drag></div>
+  <div class="box"></div>
+  <img v-lazy="img_url1" class="img" />
+  <div class="box"></div>
+  <img v-lazy="img_url2" class="img" />
 </template>
 
 <script setup>
-
+const img_url1 = "https://www.runoob.com/wp-content/uploads/2015/02/011500266295799.jpg"
+const img_url2 = "https://dss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/topnav/newfanyi-da0cea8f7e.png"
 </script>
 
 <style lang="scss">
 .box {
   width: 200px;
-  height: 200px;
+  height: 1200px;
   background-color: chocolate;
-  position: absolute;
+}
+.img {
+  width: 200px;
+  height: 200px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,35 +2,18 @@ import { createApp, onMounted } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
-app.directive('drag', {
+app.directive('lazy', {
   mounted (el, binding) {
-    el.onmousedown = (mouse) => {
-      console.log(mouse)
-      // 鼠标按下的位置
-      const mouseXStart = mouse.clientX
-      const mouseYStart = mouse.clientY
-      console.log("按下开始", mouseXStart, mouseYStart)
-      // 当前滑块位置
-      const rectLeft = el.offsetLeft
-      const rectTop = el.offsetTop
-      document.onmousemove = (e) => {
-        // 鼠标移动的位置
-        const mouseXEnd = e.clientX
-        const mouseYEnd = e.clientY
-        const moveX = mouseXEnd - mouseXStart + rectLeft
-        const moveY = mouseYEnd - mouseYStart + rectTop
-        console.log(rectLeft, rectTop)
-        el.style["top"] = moveY + "px"
-        el.style["left"] = moveX + "px"
+    var observer = new IntersectionObserver(e => {
+      console.log(e)
+      if (e[0].isIntersecting) {
+        el.src = binding.value
+        console.log(el.src)
+        observer.unobserve(el)
       }
-      document.onmouseup = () => {
-        console.log("鼠标抬起")
-        // 取消事件
-        document.onmousemove = null
-      }
-    }
-
-
+    })
+    observer.observe(el)
   }
 })
+
 app.mount('#app')
